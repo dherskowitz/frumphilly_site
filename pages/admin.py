c2da6b2644145c01bd977ec760c83ecc93680f2f
Daniel <dherskowitz@hertzfurniture.com>
2020-01-16 17:46:33 +0200

    added models for pages

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Page, PageContent
+
 
 # Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    model = Page
+    list_display = [
+        "page_name",
+    ]
+
+
+class PageContentAdmin(admin.ModelAdmin):
+    model = PageContent
+    list_display = [
+        "page",
+        "content_name",
+    ]
+
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(PageContent, PageContentAdmin)
