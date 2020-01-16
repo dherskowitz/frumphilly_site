ed6c650fddbcfd84b3df913c34943f9af0d21d49
Daniel <dherskowitz@hertzfurniture.com>
2020-01-16 17:47:00 +0200

    pass page meta from db to template

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from pages.models import Page
 
 
 def home(request):
-    return render(request, "pages/home.html")
+    page = Page.objects.get(page_name="Home")
+    context = {"meta_title": page.meta_title, "meta_description": page.meta_description}
+    return render(request, "pages/home.html", context)
