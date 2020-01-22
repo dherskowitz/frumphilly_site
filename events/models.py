771a5cfc23c0330ebc6c989b5b820050b0655150
Daniel <dherskowitz@hertzfurniture.com>
2020-01-22 15:21:13 +0200

    started on events model, added events to admin

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Event(models.Model):
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
