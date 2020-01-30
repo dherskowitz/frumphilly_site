61b940ae3d22b43ad2d6b7b5a44e80ad626e0eba
Daniel <dherskowitz@hertzfurniture.com>
2020-01-30 14:49:16 +0200

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+var path = require('path');
+// var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: ["./static/src/js/app.js", "./static/src/scss/app.scss"],
+    output: {
+        path: path.resolve("./static/dist/"),
+        filename: "[name]-[hash].js"
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "/"),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === "development"
+                        }
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: "postcss",
+                            plugins: [require("autoprefixer")]
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new BundleTracker({
+            filename: './webpack-stats.json'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            chunkFilename: '[id].[hash].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin([{
+            from: './static/src/img/**/*',
+            to: './images/[name].[ext]'
+        }, ]),
+    ]
+}
