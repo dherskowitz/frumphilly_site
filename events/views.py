2ade19195ac874a9e06571af53f63fd1bd1bad2d
Daniel <dherskowitz@gmail.com>
2020-02-02 18:07:33 +0200

    changes view names, added single and create

--- events/views.py
+++ events/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from .models import Event
+from .forms import EventForm
 
 
 # Create your views here.
-def events(request):
-    events = Event.objects.all()
+def events_all(request):
+    events = Event.objects.order_by("-start_date")
     context = {"events": events}
-    return render(request, "pages/events.html", context)
+    return render(request, "pages/events/events_all.html", context)
+
+
+def events_single(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    context = {"event": event}
+    return render(request, "pages/events/events_single.html", context)
+
+
+def events_create(request):
+    form = EventForm()
+    context = {"form": form}
+
+    if request.method == "POST":
+        form = EventForm(request)
+        context["form"] = form
+
+    return render(request, "pages/events/events_create.html", context)
