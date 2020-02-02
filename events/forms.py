732cc4d468b7b05332358427e8b97e861c447889
Daniel <dherskowitz@gmail.com>
2020-02-02 18:11:15 +0200

    added all fields, added time widget, exclude created by

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
-from . import models
+from django import forms
+from .models import Event
 
 
-class EventForm(ModelForm):
+class TimeInput(forms.TimeInput):
+    input_type = "time"
+
+
+class EventForm(forms.ModelForm):
     class Meta:
-        model = models.Event
-        fields = ["name", "image"]
+        model = Event
+        fields = "__all__"
+        exclude = ("created_by",)
+
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.fields["start_time"].widget = TimeInput()
