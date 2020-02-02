bf9441f584548433e84d1496a588f5f39224e416
Daniel <dherskowitz@gmail.com>
2020-02-02 18:07:51 +0200

    added urls for single and create

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("events", views.events, name="events"),
+    path("events", views.events_all, name="events_all"),
+    path("events/create", views.events_create, name="events_create"),
+    path("events/<int:event_id>", views.events_single, name="events_single"),
 ]
