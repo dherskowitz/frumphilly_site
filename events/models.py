7011c47196951e19a87419bf83032b359b2b722e
Daniel <dherskowitz@gmail.com>
2020-02-04 20:14:56 +0200

    compress images on form submit, formatting

--- events/models.py
+++ events/models.py
@@ -1,7 +1,23 @@
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
 from django.db import models
 from django.conf import settings
 from app.storage_backends import EventMediaStorage, EventFileStorage
 from datetime import date
+
+
+def compress(image):
+    size = 800, 800
+    im = Image.open(image)
+    # create a BytesIO object
+    im_io = BytesIO()
+    # save image to BytesIO object
+    im.thumbnail(size, Image.ANTIALIAS)
+    im.save(im_io, "JPEG", quality=70)
+    # create a django-friendly Files object
+    new_image = File(im_io, name=image.name)
+    return new_image
 
 
 # Create your models here.
@@ -17,7 +33,7 @@ class Event(models.Model):
         null=True,
         blank=True,
     )
-    image = models.FileField(
+    image = models.ImageField(
         storage=EventMediaStorage(),
         default=None,
         help_text="Upload an image or poster for this event.",
@@ -75,16 +91,35 @@ class Event(models.Model):
         blank=True,
         help_text="What time does your event start?",
     )
-    start_date = models.DateField(default=None, auto_now=False, auto_now_add=False, help_text="When will your event take place?")
+    start_date = models.DateField(
+        default=None,
+        auto_now=False,
+        auto_now_add=False,
+        help_text="When will your event take place?",
+    )
     end_date = models.DateField(
-        default=None, auto_now=False, auto_now_add=False, null=True, blank=True, help_text="If your event occurs over multiple days, when will your event end?"
+        default=None,
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+        help_text="If your event occurs over multiple days, when will your event end?",
     )
     rsvp = models.BooleanField(
-        default=None, help_text="Does your event require RSVP or tickets?"
+        default=None,
+        help_text="Does your event require RSVP or tickets?",
+        verbose_name="RSVP",
     )
     cost = models.DecimalField(
-        default=None, max_digits=8, decimal_places=2, null=True, blank=True, help_text="If anything, how much will it cost to attend your event?"
+        default=None,
+        max_digits=8,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        help_text="If anything, how much will it cost to attend your event?",
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -92,3 +127,13 @@ class Event(models.Model):
     @property
     def event_in_past(self):
         return date.today() > self.start_date
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            # call the compress function
+            new_image = compress(self.image)
+            # set self.image to new_image
+            self.image = new_image
+            # save
+            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
