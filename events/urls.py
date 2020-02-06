ff63d3d764709e063b4af80cb02df5df3a6822d4
Daniel <dherskowitz@gmail.com>
2020-02-06 15:45:59 +0200

    changed pk to slug, added edit url

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("events", views.events_all, name="events_all"),
-    path("events/create", views.events_create, name="events_create"),
-    path("events/<int:event_id>", views.events_single, name="events_single"),
+    path("events/", views.events_all, name="events_all"),
+    path("events/create/", views.events_create, name="events_create"),
+    path("events/<slug>/", views.events_single, name="events_single"),
+    path("events/<slug>/edit/", views.events_edit, name="events_edit"),
 ]
