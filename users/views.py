f007cc39f851d517a0edf7b94d60b62fbbec25d6
Daniel <dherskowitz@gmail.com>
2020-02-11 10:17:57 +0200

    added user events, see all events by current user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from events.models import Event
+
 
 # Create your views here.
+@login_required
+def user_events(request):
+    events = Event.objects.filter(created_by=request.user)
+    context = {
+        "events": events,
+    }
+    return render(request, "pages/user/user_events.html", context)
