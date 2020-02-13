135dc552ace4e832944275267302c52375e14804
Daniel <dherskowitz@gmail.com>
2020-02-13 19:12:35 +0200

    check if upcoming events

--- pages/views.py
+++ pages/views.py
@@ -10,9 +10,14 @@ def home(request):
     for page_item in page_content:
         page_data[page_item.content_name] = page_item.content
     upcoming_events = Event.get_upcoming_events()
+    events_in_future = True
+    if len(upcoming_events) == 0:
+        upcoming_events = Event.objects.all().order_by("-start_date")[:10]
+        events_in_future = False
     context = {
         "page_data": page_data,
         "page_meta": page_meta,
         "upcoming_events": upcoming_events,
+        "events_in_future": events_in_future,
     }
     return render(request, "pages/home.html", context)
