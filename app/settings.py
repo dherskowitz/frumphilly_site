fc57b8e2cf59ef60a2a60cd9635c257624f92517
Daniel <dherskowitz@gmail.com>
2020-02-13 17:46:41 +0200

    fix webpack loader settings

--- app/settings.py
+++ app/settings.py
@@ -183,7 +183,7 @@ WEBPACK_LOADER = {
 }
 
 if config("ENV") != 'local':
-    WEBPACK_LOADER.update({
+    WEBPACK_LOADER['DEFAULT'].update({
         'BUNDLE_DIR_NAME': 'dist/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prod.json')
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prod.json'
     })
