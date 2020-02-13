7531b0424b166160cddef6e3458366b32dbb2e69
Daniel <dherskowitz@gmail.com>
2020-02-13 15:04:24 +0200

    remove old db dict

--- app/settings.py
+++ app/settings.py
@@ -93,21 +93,6 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "HOST": config("DB"),
-#         "PORT": 5432,
-#     }
-# }
 DATABASES = {}
 DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 DATABASES["default"] = dj_database_url.config(
