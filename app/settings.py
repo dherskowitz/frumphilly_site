3e11f16c775c6945cf8e9b6d20c909e76719fb0c
Daniel <dherskowitz@gmail.com>
2020-02-14 10:46:45 +0200

    changed static location

--- app/settings.py
+++ app/settings.py
@@ -152,10 +152,10 @@ AWS_S3_CUSTOM_DOMAIN = AWS_CLOUDFRONT_DOMAIN
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
-AWS_STATIC_LOCATION = "assets"
+AWS_STATIC_LOCATION = "static"
 if config("ENV") != "local":
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/assets/"
 DEFAULT_FILE_STORAGE = "app.storage_backends.S3PublicMediaStorage"
 
 
