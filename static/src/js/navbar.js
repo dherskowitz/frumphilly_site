8fc267e3b7456c5fa94c8ed1446bc463f5055ba5
Daniel <dherskowitz@gmail.com>
2020-02-17 13:47:26 +0200

    toggle algolia z-index when menu open

--- static/src/js/navbar.js
+++ static/src/js/navbar.js
@@ -1,7 +1,19 @@
 const navbar__icon = document.querySelector('#navbar__icon');
 const navbar__nav = document.querySelector('#navbar__nav');
+const places = document.querySelector('.algolia-places');
+
 navbar__icon.addEventListener('click', (event) => {
     document.body.classList.toggle('body--menu-open');
     event.currentTarget.classList.toggle('navbar__icon--open');
     navbar__nav.classList.toggle('navbar__nav--open');
+    if (places) {
+        // If places field on page toggle z-index when menu is open
+        if (document.body.classList.contains("body--menu-open")) {
+            places.style.zIndex = "-1"
+        } else {
+            setTimeout(() => {
+                places.style.zIndex = "100"
+            }, 500);
+        }
+    }
 });
