f7e101b7581f7ae9356151e558e10ca4cb3dd78c
Daniel <dherskowitz@gmail.com>
2020-02-25 12:06:49 +0200

    validate file is pdf < 5mb

--- events/forms.py
+++ events/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+
+# from django.template.defaultfilters import filesizeformat
+# from django.utils.translation import ugettext_lazy as _
 from .models import Event
+
+
+CONTENT_TYPES = ["pdf"]
+# 2.5MB - 2621440
+# 5MB - 5242880
+# 10MB - 10485760
+# 20MB - 20971520
+# 50MB - 5242880
+# 100MB 104857600
+# 250MB - 214958080
+# 500MB - 429916160
+MAX_UPLOAD_SIZE = 5242880
 
 
 class TimeInput(forms.TimeInput):
@@ -39,6 +54,7 @@ class EventForm(forms.ModelForm):
             "pattern"
         ] = "[0-9]{3}-[0-9]{3}-[0-9]{4}"
         self.fields["image"].widget.attrs["accept"] = "image/png,image/jpg,image/jpeg"
+        self.fields["attachment"].widget.attrs["accept"] = "application/pdf"
 
         # Set placeholder for each field
         for field in self.fields:
@@ -51,4 +67,19 @@ class EventForm(forms.ModelForm):
         self.fields["phone_contact"].widget.attrs[
             "placeholder"
         ] = "expects 999-999-9999"
+        self.fields["link"].widget.attrs[
+            "placeholder"
+        ] = "requires full URL e.g. http://example.com"
+        self.fields["cost"].widget.attrs[
+            "placeholder"
+        ] = "If your event has a cost to attend."
 
+    def clean_attachment(self):
+        attachment = self.cleaned_data["attachment"]
+        content_type = attachment.content_type.split("/")[1]
+        if content_type in CONTENT_TYPES:
+            if attachment.size > MAX_UPLOAD_SIZE:
+                raise forms.ValidationError("Attachment file must be less than 5MB.")
+        else:
+            raise forms.ValidationError("Attachment only accepts PDF files.")
+        return attachment
