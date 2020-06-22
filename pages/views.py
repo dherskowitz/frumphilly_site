3472339537ad2ecdcd10d3074f1813531a52b7d4
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-06-22 15:39:19 +0300

    remove static content from view

--- pages/views.py
+++ pages/views.py
@@ -4,19 +4,19 @@ from events.models import Event
 
 
 def home(request):
-    page_meta = Page.objects.get(page_name="Home")
-    page_content = PageContent.objects.filter(page=page_meta.id)
-    page_data = {}
-    for page_item in page_content:
-        page_data[page_item.content_name] = page_item.content
+    # page_meta = Page.objects.get(page_name="Home")
+    # page_content = PageContent.objects.filter(page=page_meta.id)
+    # page_data = {}
+    # for page_item in page_content:
+    #     page_data[page_item.content_name] = page_item.content
     upcoming_events = Event.get_upcoming_events()
     events_in_future = True
     if len(upcoming_events) == 0:
         upcoming_events = Event.objects.all().order_by("-start_date")[:10]
         events_in_future = False
     context = {
-        "page_data": page_data,
-        "page_meta": page_meta,
+        # "page_data": page_data,
+        # "page_meta": page_meta,
         "upcoming_events": upcoming_events,
         "events_in_future": events_in_future,
     }
