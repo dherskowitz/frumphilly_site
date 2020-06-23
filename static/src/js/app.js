9442264f246741719c5770b24386cae0b653043e
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-06-23 14:49:05 +0300

    prevent animation on resize, require auth

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,2 +1,12 @@
-require('./messages');
-require('./navbar');
+require("./messages");
+require("./navbar");
+require("./auth");
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
