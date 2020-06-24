c88f24b08bb729e52cdc53b0a621d70e5bfc2c06
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-06-24 10:37:59 +0300

    added terms/privacy pages & links

--- pages/views.py
+++ pages/views.py
@@ -25,3 +25,11 @@ def home(request):
 
 def about(request):
     return render(request, "pages/about.html")
+
+
+def terms(request):
+    return render(request, "pages/terms.html")
+
+
+def privacy(request):
+    return render(request, "pages/privacy.html")
