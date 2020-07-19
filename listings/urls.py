b1059541502c0514a65790f99429ac70f333dcc9
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-07-19 18:12:34 +0300

    add single listing

--- listings/urls.py
+++ listings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.listings, name="listings"),
+    path("<slug:slug>-<int:pk>", views.listing_single, name="listing_single"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 
 
@@ -6,3 +6,10 @@ def listings(request):
     listings = Listing.get_listings()
     context = {"listings": listings}
     return render(request, "listings/index.html", context)
+
+
+def listing_single(request, slug, pk):
+    print(slug, pk)
+    listing = get_object_or_404(Listing, slug=slug, id=pk)
+    context = {"listing": listing}
+    return render(request, "listings/single.html", context)
