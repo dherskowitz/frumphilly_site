e09771faac2b54f07e830fe89cec31ed2dc6dc6f
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-08-10 18:43:31 +0300

    add listings pages + crud

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Listing
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from .models import Listing, Category
+from .forms import ListingForm
 
 
 def listings(request):
@@ -9,7 +13,81 @@ def listings(request):
 
 
 def listing_single(request, slug, pk):
-    print(slug, pk)
     listing = get_object_or_404(Listing, slug=slug, id=pk)
     context = {"listing": listing}
     return render(request, "listings/single.html", context)
+
+
+def listings_category(request, slug):
+    category = Category.get_category_by_slug(slug)
+    listings = Listing.get_listings_by_category(slug)
+    context = {"listings": listings, "category": category}
+    return render(request, "listings/category.html", context)
+
+
+@login_required
+def listings_create(request):
+    form = ListingForm()
+    check_fields = [
+        "claimed",
+        "premium",
+        "approved",
+        "accept_cc",
+        "delivers",
+        "wheelchair_access",
+    ]
+    context = {"form": form, "check_fields": check_fields}
+    if request.method == "POST":
+        form = ListingForm(request.POST, request.FILES)
+        context["form"] = form
+
+        if form.is_valid():
+            listing = form.save(commit=False)
+            listing.created_by = request.user
+            listing.save()
+            messages.success(request, "Listing created successfully!")
+            return redirect(listing_single, slug=listing.slug, pk=listing.id)
+
+    return render(request, "listings/create.html", context)
+
+
+@login_required
+def listings_edit(request, slug, pk):
+    listing = get_object_or_404(Listing, slug=slug, id=pk)
+    if request.user != listing.created_by:
+        return HttpResponseForbidden()
+    form = ListingForm(instance=listing)
+    check_fields = [
+        "claimed",
+        "premium",
+        "approved",
+        "accept_cc",
+        "delivers",
+        "wheelchair_access",
+    ]
+    context = {"form": form, "check_fields": check_fields, "listing": listing}
+    if request.method == "POST":
+        form = ListingForm(request.POST, request.FILES, instance=listing)
+        context["form"] = form
+
+        if form.is_valid():
+            listing = form.save(commit=False)
+            listing.created_by = request.user
+            listing.save()
+            form.save_m2m()
+            messages.success(request, "Listing updated successfully!")
+            return redirect(listing_single, slug=listing.slug, pk=listing.id)
+
+    return render(request, "listings/edit.html", context)
+
+
+@login_required
+def listings_delete(request, slug, pk):
+    listing = get_object_or_404(Listing, slug=slug, id=pk)
+    if request.user != listing.created_by:
+        return HttpResponseForbidden()
+    context = {"listing": listing}
+    if request.method == "POST":
+        listing.delete()
+        return redirect(listings)
+    return render(request, "listings/delete.html", context)
