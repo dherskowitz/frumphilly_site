1336a560b47215fc04231c24fabc400b53acd530
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-08-10 18:47:33 +0300

    add listings to user management

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("user/account/", views.user_account, name="user_account",),
     path("user/events/", views.user_events, name="user_events",),
+    path("user/listings/", views.user_listings, name="user_listings",),
 ]
