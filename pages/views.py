d99c8115c7298ded22e50c22b9009b6e7a1a318a
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-08-11 16:16:15 +0300

    remove commented code

--- pages/views.py
+++ pages/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from events.models import Event
 
 
 def home(request):
-    # page_meta = Page.objects.get(page_name="Home")
-    # page_content = PageContent.objects.filter(page=page_meta.id)
-    # page_data = {}
-    # for page_item in page_content:
-    #     page_data[page_item.content_name] = page_item.content
     upcoming_events = Event.get_upcoming_events()
     events_in_future = True
     if len(upcoming_events) == 0:
         upcoming_events = Event.objects.all().order_by("-start_date")[:10]
         events_in_future = False
     context = {
-        # "page_data": page_data,
-        # "page_meta": page_meta,
         "upcoming_events": upcoming_events,
         "events_in_future": events_in_future,
     }
@@ -28,7 +20,7 @@ def about(request):
 
 
 def contact(request):
-    return HttpResponse("Contact page")
+    return render(request, "pages/site/contact.html")
 
 
 def terms(request):
