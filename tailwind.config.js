1c741a9da54b42d8ef69fd59bf3aa05b43d3dbc7
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-08-26 13:00:17 +0300

    formatting, add custom zIndex/fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,54 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {
-      colors: {
-        bgGrey: 'rgba(210, 215, 211, 1)',
-      },
-      height: {
-        25: '25vh',
-        40: '40vh',
-        50: '50vh',
-        60: '60vh',
-        100: '100vh'
-      },
-      inset: {
-        'avoid-header': '4rem',
-        '-2': '-2rem',
-        '-4': '-3.5rem'
-      }
-    },
-    textShadow: {
-      'default': '2px 2px 3px #000',
-    },
-    zIndex: {
-      '-1': '-1',
-    }
-  },
-  variants: {
-    textColor: ['group-hover'],
-  },
-  plugins: [
-    plugin(function ({
-      addUtilities
-    }) {
-      const addShadows = {
-        '.text-shadow': {
-          'text-shadow': '2px 2px 3px #000',
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                bgGrey: 'rgba(210, 215, 211, 1)',
+            },
+            height: {
+                25: '25vh',
+                40: '40vh',
+                50: '50vh',
+                60: '60vh',
+                100: '100vh'
+            },
+            inset: {
+                'avoid-header': '4rem',
+                '-2': '-2rem',
+                '-4': '-3.5rem'
+            },
+            zIndex: {
+                '-1': '-1',
+            },
+            fontSize: {
+                '4.5xl': '2.5rem'
+            }
         },
-      }
-      addUtilities(addShadows)
-    })
-  ],
+        textShadow: {
+            'default': '2px 2px 3px #000',
+        }
+    },
+    variants: {
+        textColor: ['group-hover'],
+        textShadow: ['responsive'],
+    },
+    plugins: [
+        plugin(function({
+            addUtilities
+        }) {
+            const addShadows = {
+                '.text-shadow': {
+                    'text-shadow': '2px 2px 3px #000',
+                },
+                '.text-shadow-0': {
+                    'text-shadow': 'none',
+                },
+            }
+            addUtilities(addShadows, {
+                variants: ['responsive'],
+            })
+        })
+    ],
 }
