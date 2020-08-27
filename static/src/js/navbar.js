d799b05059b0379f0e667ddd291927e66056cd6b
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-08-27 19:15:28 +0300

    use new class/id

--- static/src/js/navbar.js
+++ static/src/js/navbar.js
@@ -1,11 +1,11 @@
 const navbar__icon = document.querySelector('#navbar__icon');
-const navbar__nav = document.querySelector('#navbar__nav');
+const menu = document.querySelector('#mobile_nav');
 const places = document.querySelector('.algolia-places');
 
 navbar__icon.addEventListener('click', (event) => {
-    document.body.classList.toggle('body--menu-open');
+    // document.body.classList.toggle('body--menu-open');
     event.currentTarget.classList.toggle('navbar__icon--open');
-    navbar__nav.classList.toggle('navbar__nav--open');
+    menu.classList.toggle('mobile_nav--open');
     if (places) {
         // If places field on page toggle z-index when menu is open
         if (document.body.classList.contains("body--menu-open")) {
