c77c95257b7fa11487717921a944dda165bbbf23
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-09-02 18:34:41 +0300

    add user settings

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("user/settings/", views.user_settings, name="user_settings",),
     path("user/account/", views.user_account, name="user_account",),
     path("user/events/", views.user_events, name="user_events",),
     path("user/listings/", views.user_listings, name="user_listings",),
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from events.models import Event
 from listings.models import Listing
 from .models import CustomUser
+from .forms import CustomUserSettingsForm
 
 
 # Create your views here.
@@ -13,6 +15,22 @@ def user_account(request):
         "listings_count": CustomUser.get_listings_count(),
     }
     return render(request, "user/user_account.html", context)
+
+
+@login_required
+def user_settings(request):
+    user = CustomUser.get_user_settings(request.user)
+    context = {"form": CustomUserSettingsForm(instance=user)}
+    if request.method == "POST":
+        form = CustomUserSettingsForm(request.POST, request.FILES, instance=user)
+        context["form"] = form
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            form.save_m2m()
+            messages.success(request, "User updated successfully!")
+            return redirect("user_settings")
+    return render(request, "user/user_settings.html", context)
 
 
 @login_required
