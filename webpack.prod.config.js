5460a182f89d98d50c239ca2159b960f6b98b528
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-09-07 23:10:32 +0300

    run prod build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,33 +1,72 @@
-const config = require('./webpack.config.js');
-const BundleTracker = require('webpack-bundle-tracker');
+const config = require("./webpack.config.js");
+const BundleTracker = require("webpack-bundle-tracker");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CopyPlugin = require('copy-webpack-plugin');
-// const path = require('path')
-const glob = require('glob')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
+const glob = require("glob");
 const PurgecssPlugin = require("purgecss-webpack-plugin");
 
-config.output.path = require('path').resolve('./static/dist');
-
-config.plugins = [
-    new BundleTracker({
-        filename: './webpack-stats-prod.json'
-    }),
-    new PurgecssPlugin({
-        paths: glob.sync(`templates/**/*`, {
-            nodir: true
-        })
-    }),
-    new MiniCssExtractPlugin({
-        filename: '[name].[hash].css',
-        chunkFilename: '[id].[hash].css',
-        ignoreOrder: false
-    }),
-    // new CopyPlugin([{
-    //     from: './static/src/libs/**/*',
-    //     to: './libs/[name].[ext]'
-    // }, ]),
-]
-
-// override any other settings here like using Uglify or other things that make sense for production environments.
-
-module.exports = config;
+module.exports = {
+    entry: ["./static/src/js/app.js", "./static/src/main.css"],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+            }),
+            new OptimizeCSSAssetsPlugin({
+                sourceMap: true,
+            }),
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, "static/dist"),
+        filename: "[name]-[hash].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: "postcss",
+                            plugins: [
+                                require("tailwindcss"),
+                                require("autoprefixer"),
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new BundleTracker({
+            filename: "./webpack-stats-prod.json",
+        }),
+        new PurgecssPlugin({
+            paths: glob.sync(`templates/**/*`, {
+                nodir: true,
+            }),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css",
+            chunkFilename: "[id].[hash].css",
+            ignoreOrder: false,
+        }),
+    ],
+};
