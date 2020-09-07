e52a60cba3eee6b4523147dfbfe6afe5f7d799b0
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-09-07 13:58:58 +0300

    change url param to slug

--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.listings, name="listings"),
-    path("create/<str:listing_type>/", views.listings_create, name="listings_create"),
+    path("create/<slug:slug>/", views.listings_create, name="listings_create"),
     path("select/", views.listings_select, name="listings_select"),
     path("<slug:slug>-<int:pk>/", views.listing_single, name="listing_single"),
     path("<slug:slug>-<int:pk>/edit/", views.listings_edit, name="listings_edit"),
