4f8c6e3e4c5e41200d48afa0f56267ae4bcca338
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-09-08 11:07:12 +0300

    run prod build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,65 +4,72 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
-const glob = require("glob");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
+// const glob = require("glob");
+// const PurgecssPlugin = require("purgecss-webpack-plugin");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
 
 module.exports = {
     entry: ["./static/src/js/app.js", "./static/src/main.css"],
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                sourceMap: true,
-            }),
-            new OptimizeCSSAssetsPlugin({
-                sourceMap: true,
-            }),
-        ],
-    },
+    // optimization: {
+    //     minimizer: [
+    //         // new TerserPlugin({
+    //         //     sourceMap: true,
+    //         // }),
+    //         // new OptimizeCSSAssetsPlugin({
+    //         //     sourceMap: true,
+    //         // }),
+    //     ],
+    // },
     output: {
         path: path.resolve(__dirname, "static/dist"),
         filename: "[name]-[hash].js",
     },
     module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            sourceMap: true,
-                        },
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        sourceMap: true,
                     },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true,
-                        },
+                },
+                {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: true,
                     },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            ident: "postcss",
-                            plugins: [
-                                require("tailwindcss"),
-                                require("autoprefixer"),
-                            ],
-                        },
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        ident: "postcss",
+                        plugins: [
+                            require("tailwindcss"),
+                            require("autoprefixer"),
+                            cssnano({
+                                preset: "default",
+                            }),
+                            purgecss({
+                                content: ["./templates/**/*.html"],
+                                defaultExtractor: (content) =>
+                                    content.match(/[\w-/:]+(?<!:)/g) || [],
+                            }),
+                        ],
                     },
-                ],
-            },
-        ],
+                },
+            ],
+        }, ],
     },
     plugins: [
         new BundleTracker({
             filename: "./webpack-stats-prod.json",
         }),
-        new PurgecssPlugin({
-            paths: glob.sync(`templates/**/*`, {
-                nodir: true,
-            }),
-        }),
+        // new PurgecssPlugin({
+        //     paths: glob.sync(`templates/**/*`, {
+        //         nodir: true,
+        //     }),
+        // }),
         new MiniCssExtractPlugin({
             filename: "[name].[hash].css",
             chunkFilename: "[id].[hash].css",
