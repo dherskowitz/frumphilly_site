0209536a614847a2c190e5603c4177f56fe7334a
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-10-29 10:49:43 +0200

    add blank line before function

--- events/templatetags/my_tags.py
+++ events/templatetags/my_tags.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def param_replace(context, **kwargs):
     d = context['request'].GET.copy()
