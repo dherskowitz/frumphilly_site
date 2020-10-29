9bac9bb60a549dde105b956ecc1085aa3566975c
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-10-29 10:38:25 +0200

    show listings on homepage

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from events.models import Event
+from listings.models import Listing
 
 
 def home(request):
+    listings = Listing.objects.all()[:9]
     upcoming_events = Event.get_upcoming_events()
     events_in_future = True
     if len(upcoming_events) == 0:
         upcoming_events = Event.objects.all().order_by("-start_date")[:9]
         events_in_future = False
     context = {
+        "listings": listings,
         "upcoming_events": upcoming_events,
         "events_in_future": events_in_future,
     }
