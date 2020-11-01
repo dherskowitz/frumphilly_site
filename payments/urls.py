db426788a7bf5142f713f2611ecb4ef3cf30b494
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-11-01 11:29:29 +0200

    add customer info to session, add webhook

--- payments/urls.py
+++ payments/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('cancelled/', views.payments_cancelled, name='payments_cancelled'),
     path('config/', views.stripe_config, name='stripe_config'),
     path('create-checkout-session/', views.create_checkout_session),
+    path('webhook/', views.stripe_webhook, name='stripe_webhook'),
 ]
