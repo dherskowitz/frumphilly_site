83dba6b981a58ba95a12e280a165b31e837db031
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-11-02 17:50:31 +0200

    reorder urls to avoid collision

--- listings/urls.py
+++ listings/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<slug:slug>-<int:pk>/", views.listing_single, name="listing_single"),
-    path("<slug:slug>/", views.listings, name="listings"),
-    path("create/<slug:slug>/", views.listings_create, name="listings_create"),
     path("select/", views.listings_select, name="listings_select"),
+    path("create/<slug:slug>/", views.listings_create, name="listings_create"),
     path("<slug:slug>-<int:pk>/edit/", views.listings_edit, name="listings_edit"),
     path("<slug:slug>-<int:pk>/delete/", views.listings_delete, name="listings_delete"),
+    path("<slug:slug>-<int:pk>/", views.listing_single, name="listing_single"),
+    path("<slug:slug>/", views.listings, name="listings"),
 ]
