b1a9fe647fd6a0db3d6d71fdb2ecf86e1ec3551a
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2020-11-10 11:08:02 +0200

    remove ssl settings

--- app/settings.py
+++ app/settings.py
@@ -256,8 +256,6 @@ if config("ENV") != "local":
     )
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Override Message Tags
