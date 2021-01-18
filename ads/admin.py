566ec41698b1727d6b9de22591dc2effbe816f99
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-18 20:49:02 +0200

    add display fields

--- ads/admin.py
+++ ads/admin.py
@@ -3,9 +3,9 @@ from .models import Ad
 
 
 class AdAdmin(admin.ModelAdmin):
-    list_display = ("title", "contract_length", "status")
+    list_display = ("title", "contract_length", "status", "type")
     list_filter = ("contract_length", "status", "type")
-    # readonly_fields = ("slug",)
+    read_only = ("redirect_uuid")
 
 
 admin.site.register(Ad, AdAdmin)
