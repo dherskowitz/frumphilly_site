b08c496ef05f7915201daa19f65247a24d31a8f4
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-20 18:39:38 +0200

    add payments model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    amount_paid = models.IntegerField()
+    stripe_checkout_id = models.CharField(max_length=254)
+    email = models.CharField(max_length=254)
+    payment_status = models.CharField(max_length=50)
+    purchase_type = models.CharField(max_length=50)
+    purchase_choice = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="payments",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "payment"
+        verbose_name_plural = "payments"
+
+    def __str__(self):
+        return self.purchase_type + ': ' + self.purchase_choice
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
