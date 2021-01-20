1ef44a3230a5c69eb150f26856c9bea9facbac28
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-20 18:40:15 +0200

    add ad payment checkout, continue on webhook

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,11 @@
 import stripe
+import json
 from django.shortcuts import render
 from decouple import config
 from django.http.response import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from ads.models import Ad, ad_prices
+from .models import Payment
 
 
 # config("SECRET_KEY")
@@ -22,17 +25,48 @@ def payments_cancelled(request):
 def stripe_config(request):
     if request.method == "GET":
         stripe_config = {"public_key": config("STRIPE_PUBLISHABLE_KEY")}
-        return JsonResponse(stripe_config, safe=False)
+        return stripe_config
+        # return JsonResponse(stripe_config, safe=False)
+
+
+# @csrf_exempt
+# def create_checkout_session(request):
+#     if request.method == "GET":
+#         domain_url = config("DOMAIN_URL")
+#         stripe.api_key = config("STRIPE_SECRET_KEY")
+#         try:
+#             checkout_session = stripe.checkout.Session.create(
+#                 customer_email=request.user.email,
+#                 client_reference_id=request.user.id
+#                 if request.user.is_authenticated
+#                 else None,
+#                 success_url=domain_url
+#                 + "/payments/success?session_id={CHECKOUT_SESSION_ID}",
+#                 cancel_url=f"{domain_url}/payments/cancelled/",
+#                 payment_method_types=["card"],
+#                 mode="payment",
+#                 # line_items=[{
+#                 #     'price': 'price_1Hhfk5G3pTnNZwrhlGv4DMHY',
+#                 #     'quantity': 1,
+#                 # }],
+#             )
+#             return JsonResponse({"sessionId": checkout_session["id"]})
+#         except Exception as e:
+#             return JsonResponse({"error": str(e)})
 
 
 @csrf_exempt
-def create_checkout_session(request):
+def create_ad_checkout_session(request):
+    uuid = request.GET['ad']
+    ad = Ad.objects.filter(redirect_uuid=uuid).first()
+    price = ad_prices[f"{ad.contract_length}"]
+
     if request.method == "GET":
         domain_url = config("DOMAIN_URL")
         stripe.api_key = config("STRIPE_SECRET_KEY")
         try:
             checkout_session = stripe.checkout.Session.create(
-                customer_email=request.user.email,
+                # customer_email=request.user.email,
                 client_reference_id=request.user.id
                 if request.user.is_authenticated
                 else None,
@@ -41,10 +75,18 @@ def create_checkout_session(request):
                 cancel_url=f"{domain_url}/payments/cancelled/",
                 payment_method_types=["card"],
                 mode="payment",
-                line_items=[{
-                    'price': 'price_1Hhfk5G3pTnNZwrhlGv4DMHY',
-                    'quantity': 1,
-                }],
+                metadata={
+                    "purchase_type": "ad",
+                    "uuid": uuid
+                },
+                line_items=[
+                    {
+                        'name': f'{ad.type.title()} Ad',
+                        'quantity': 1,
+                        'currency': 'usd',
+                        'amount': f'{price}00',
+                    }
+                ],
             )
             return JsonResponse({"sessionId": checkout_session["id"]})
         except Exception as e:
@@ -67,10 +109,31 @@ def stripe_webhook(request):
         return HttpResponse(status=400)
 
     if event["type"] == "checkout.session.completed":
-        # pass
-        print("==============")
-        import pprint
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(payload)
+        # convert payload to dict 
+        response = json.loads(payload)
+
+        # get payment info from response
+        stripe_checkout_id = response['id']
+        amount_paid = response['data']['object']['amount_total']
+        email = response['data']['object']['customer_details']['email']
+        payment_status = response['data']['object']['payment_status']
+        purchase_type = response['data']['object']['metadata']['purchase_type']
+        uuid = response['data']['object']['metadata']['uuid']
+
+        # save info to local payments table
+        if purchase_type == 'ad':
+            ad = Ad.objects.get(redirect_uuid=uuid)
+            ad.status = "review"
+            ad.save()
+
+            p = Payment()
+            p.stripe_checkout_id = stripe_checkout_id
+            p.amount_paid = amount_paid
+            p.email = email
+            p.payment_status = payment_status
+            p.purchase_type = purchase_type
+            p.purchase_choice = ad.type
+            p.user = ad.user
+            p.save()
 
     return HttpResponse(status=200)
