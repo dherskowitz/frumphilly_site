0174708599d83d3114484e58676d688ca0b38db2
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-20 18:38:49 +0200

    add review url

--- ads/urls.py
+++ ads/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("out/<slug:id>/", views.redirect_ad, name="redirect_ad"),
     path("ads/create/", views.create_ad, name="create_ad"),
+    path("ads/review/", views.review_ad, name="review_ad"),
 ]
