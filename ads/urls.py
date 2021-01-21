22c821ab239e5f5b80c5b0e5677fecc4aba90541
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-21 12:05:29 +0200

    add edit ad

--- ads/urls.py
+++ ads/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("out/<slug:id>/", views.redirect_ad, name="redirect_ad"),
     path("ads/create/", views.create_ad, name="create_ad"),
+    path("ads/edit/<slug:uuid>/", views.edit_ad, name="edit_ad"),
     path("ads/review/", views.review_ad, name="review_ad"),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse
-from .models import Ad
+from .models import Ad, ad_prices
 from .forms import AdForm
 from payments.views import stripe_config
 
@@ -29,13 +29,40 @@ def create_ad(request):
 
 
 @login_required
+def edit_ad(request, uuid):
+    ad = get_object_or_404(Ad, redirect_uuid=uuid)
+    if request.user != ad.user:
+        return render(request, "403.html")
+
+    form = AdForm(instance=ad)
+    context = {
+        "form": form,
+        "ad": ad
+    }
+
+    if request.method == "POST":
+        form = AdForm(request.POST, request.FILES, instance=ad)
+        context["form"] = form
+
+        if form.is_valid():
+            ad = form.save(commit=False)
+            ad.user = request.user
+            ad.save()
+            return redirect(f"/ads/review/?ad={ad.redirect_uuid}")
+
+    return render(request, 'ads/edit.html', context)
+
+
+@login_required
 def review_ad(request):
     uuid = request.GET['ad']
     ad = Ad.objects.filter(redirect_uuid=uuid).first()
+    price_info = ad_prices[f"{ad.contract_length}"]
     if request.user != ad.user:
         return render(request, "403.html")
     context = {
         "ad": ad,
-        "stripe": stripe_config(request)
+        "stripe": stripe_config(request),
+        "price_info": price_info
     }
     return render(request, 'ads/review.html', context)
