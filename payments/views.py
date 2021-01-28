66f43e560ba57ea6320d234546f3959e88da624d
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-28 11:00:24 +0200

    remove unused code

--- payments/views.py
+++ payments/views.py
@@ -26,33 +26,6 @@ def stripe_config(request):
     if request.method == "GET":
         stripe_config = {"public_key": config("STRIPE_PUBLISHABLE_KEY")}
         return stripe_config
-        # return JsonResponse(stripe_config, safe=False)
-
-
-# @csrf_exempt
-# def create_checkout_session(request):
-#     if request.method == "GET":
-#         domain_url = config("DOMAIN_URL")
-#         stripe.api_key = config("STRIPE_SECRET_KEY")
-#         try:
-#             checkout_session = stripe.checkout.Session.create(
-#                 customer_email=request.user.email,
-#                 client_reference_id=request.user.id
-#                 if request.user.is_authenticated
-#                 else None,
-#                 success_url=domain_url
-#                 + "/payments/success?session_id={CHECKOUT_SESSION_ID}",
-#                 cancel_url=f"{domain_url}/payments/cancelled/",
-#                 payment_method_types=["card"],
-#                 mode="payment",
-#                 # line_items=[{
-#                 #     'price': 'price_1Hhfk5G3pTnNZwrhlGv4DMHY',
-#                 #     'quantity': 1,
-#                 # }],
-#             )
-#             return JsonResponse({"sessionId": checkout_session["id"]})
-#         except Exception as e:
-#             return JsonResponse({"error": str(e)})
 
 
 @csrf_exempt
