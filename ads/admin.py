4479126e18f22e11a981dd2db55df3b7b4a73197
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-28 11:16:49 +0200

    include clicks, make readonly

--- ads/admin.py
+++ ads/admin.py
@@ -3,9 +3,9 @@ from .models import Ad
 
 
 class AdAdmin(admin.ModelAdmin):
-    list_display = ("title", "contract_length", "status", "type")
+    list_display = ("title", "contract_length", "status", "type", "clicks")
     list_filter = ("contract_length", "status", "type")
-    readonly_fields = ("redirect_uuid", "created_at", "image_type")
+    readonly_fields = ("redirect_uuid", "created_at", "image_type", "clicks")
 
 
 admin.site.register(Ad, AdAdmin)
