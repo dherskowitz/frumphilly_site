1719f28db843410ed027deb853bb492d891dab03
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-29 16:19:28 +0200

    add activated_at/terms to readonly

--- ads/admin.py
+++ ads/admin.py
@@ -5,7 +5,7 @@ from .models import Ad
 class AdAdmin(admin.ModelAdmin):
     list_display = ("title", "contract_length", "status", "type", "clicks")
     list_filter = ("contract_length", "status", "type")
-    readonly_fields = ("redirect_uuid", "created_at", "image_type", "clicks")
+    readonly_fields = ("redirect_uuid", "created_at", "image_type", "clicks", "activated_at", "read_terms")
 
 
 admin.site.register(Ad, AdAdmin)
