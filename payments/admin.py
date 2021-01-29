ff3202b175f51a9c5ec94e90d1feb853e133b2cb
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-01-29 16:20:18 +0200

    add payments to admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("ad_uuid", "email", "payment_status", "purchase_type", "purchase_choice")
+    list_filter = ("payment_status", "purchase_type", "purchase_choice")
+
+
+admin.site.register(Payment, PaymentAdmin)
