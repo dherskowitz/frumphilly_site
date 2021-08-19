44f465ae575941474b1d5bfb5765d4a02586a6a1
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-08-19 13:48:29 +0300

    set up admin for forum

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ForumCategory, ForumThread, ForumPost
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+    readonly_fields = ("slug",)
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+
+
+admin.site.register(ForumCategory, CategoryAdmin)
+admin.site.register(ForumThread, ThreadAdmin)
+admin.site.register(ForumPost)
