e276dc4287a58c6c63fa08b21e184356a35027da
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-08-20 15:09:14 +0300

    add pagination

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from forum.models import ForumCategory, ForumThread, ForumPost
 
@@ -15,6 +15,16 @@ def forum_category(request, category):
     # TODO paginate threads
     threads = ForumThread.objects.filter(category__slug=category).all()
     c = ForumCategory.objects.get(slug=category)
+
+    page = request.GET.get("page", 1)
+    paginator = Paginator(threads, 10)
+    try:
+        threads = paginator.page(page)
+    except PageNotAnInteger:
+        threads = paginator.page(1)
+    except EmptyPage:
+        threads = paginator.page(paginator.num_pages)
+
     context = {
         "category": c,
         "threads": threads
@@ -27,6 +37,16 @@ def forum_thread(request, category, thread):
     t = ForumThread.objects.get(slug=thread)
     c = ForumCategory.objects.get(slug=category)
     posts = ForumPost.objects.filter(thread=t.id).order_by("created_at").all()
+
+    page = request.GET.get("page", 1)
+    paginator = Paginator(posts, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {
         "category": c,
         "thread": t,
