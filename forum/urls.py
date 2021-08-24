dc214017f0d5207c4e8803c34fcb9a2637bc8a67
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-08-24 11:07:08 +0300

    move user posts path to top

--- forum/urls.py
+++ forum/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("u/<str:user>/", views.forum_user_posts, name="forum_user_posts"),
     path("<slug:category>/add/", views.forum_thread_create, name="forum_thread_create"),
     path("<slug:category>/<slug:thread>/add", views.forum_post_create, name="forum_post_create"),
     path("<slug:category>/<slug:thread>/", views.forum_thread, name="forum_thread"),
     path("<slug:category>/", views.forum_category, name="forum_category"),
-    path("<str:user>/", views.forum_user_posts, name="forum_user_posts"),
     path("", views.forum, name="forum"),
 ]
