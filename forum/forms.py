c9f6b7dcdeb57df442d1a43d8a0dd68b64772322
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-08-24 18:55:43 +0300

    bleach content, move tags, add attrs

--- forum/forms.py
+++ forum/forms.py
@@ -6,9 +6,37 @@ FILE_CONTENT_TYPES = ["pdf"]
 IMAGE_CONTENT_TYPES = ["image"]
 MAX_UPLOAD_SIZE = 5242880
 
+allowed_tags = [
+    "h1",
+    "p",
+    "div",
+    "a",
+    "abbr",
+    "acronym",
+    "b",
+    "blockquote",
+    "table",
+    "tbody",
+    "tr",
+    "td",
+    "code",
+    "em",
+    "i",
+    "li",
+    "ol",
+    "strong",
+    "ul",
+    "br",
+    "del",
+    "pre",
+    "img",
+    "s",
+    "u",
+]
+allowed_attrs = {'a': ['href', 'rel'], 'img': ['src', 'alt', 'style']}
+
 
 class ThreadCreateForm(forms.ModelForm):
-
     class Meta:
         model = ForumThread
         fields = "__all__"
@@ -17,9 +45,13 @@ class ThreadCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ThreadCreateForm, self).__init__(*args, **kwargs)
 
+    def clean_content(self):
+        content = self.cleaned_data["content"]
+        clean_content = bleach.clean(content, tags=allowed_tags, attributes=allowed_attrs)
+        return clean_content
+
 
 class PostCreateForm(forms.ModelForm):
-
     class Meta:
         model = ForumPost
         fields = "__all__"
@@ -29,30 +61,6 @@ class PostCreateForm(forms.ModelForm):
         super(PostCreateForm, self).__init__(*args, **kwargs)
 
     def clean_content(self):
-        tags = [
-            "h1",
-            "p",
-            "div",
-            "a",
-            "abbr",
-            "acronym",
-            "b",
-            "blockquote",
-            "table",
-            "tbody",
-            "tr",
-            "td",
-            "code",
-            "em",
-            "i",
-            "li",
-            "ol",
-            "strong",
-            "ul",
-            "br",
-            "del",
-            "pre",
-        ]
         content = self.cleaned_data["content"]
-        clean_content = bleach.clean(content, tags=tags, attributes=["href"])
+        clean_content = bleach.clean(content, tags=allowed_tags, attributes=allowed_attrs)
         return clean_content
