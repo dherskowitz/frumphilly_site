cc78a3087e30d6a1841287697dde63fbf9f020d4
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-12-15 15:04:38 +0200

    move management views to new app

--- ads/urls.py
+++ ads/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path("ads/edit/<slug:uuid>/", views.edit_ad, name="edit_ad"),
     path("ads/review/", views.review_ad, name="review_ad"),
     path("ad/activate/<slug:uuid>/", views.activate_ad, name="activate_ad"),
-    path("managment/review-ads/", views.admin_review_ads, name="admin_review_ads",),
-    path("managment/review-ad/<slug:uuid>/", views.admin_review_ad, name="admin_review_ad",),
-    path("managment/ads/", views.admin_all_ads, name="admin_all_ads",),
 ]
--- ads/views.py
+++ ads/views.py
@@ -88,35 +88,3 @@ def activate_ad(request, uuid):
 
     messages.success(request, f'Ad "{ad.title}" was set to active!')
     return redirect("/manage/review-ads/")
-
-
-@login_required
-def admin_review_ads(request):
-    ads = Ad.objects.filter(status='review').order_by("-created_at")
-
-    context = {
-        "ads": ads,
-    }
-    return render(request, "admin/ad_review_all.html", context)
-
-
-@login_required
-def admin_review_ad(request, uuid):
-    ad = Ad.objects.get(redirect_uuid=uuid)
-    payments = Payment.objects.filter(ad_uuid=ad.redirect_uuid)
-    price_info = ad_prices[f"{ad.contract_length}"]
-
-    context = {
-        "ad": ad,
-        "payments": payments,
-        "price_info": price_info
-    }
-    return render(request, "admin/ad_review_single.html", context)
-
-
-@login_required
-def admin_all_ads(request):
-    context = {
-        "ads": Ad.objects.all().order_by("-created_at")
-    }
-    return render(request, "admin/ads_all.html", context)
